effbb2ffa219861639660273ed1f446cf6b1e8fb
GTrunSec <gtrunsec@hardenedlinux.org>
2022-05-14 23:52:48 -0700

    Update

--- org/.dir-locals.el
+++ org/.dir-locals.el
@@ -1,6 +1,6 @@
 ;;; .dir-locals.el --- description -*- lexical-binding: t; -*-
-((nil .  ((eval . (setq-local org-attach-id-dir  "./attach"
-                              org-roam-db-location "../.cache/org-roam.db"
-                              org-roam-directory (expand-file-name "./")))
- )))
+((nil . ((org-attach-id-dir . "./attach")
+         (org-roam-db-location . "../.cache/org-roam.db")
+         (org-roam-directory . "./")
+         )))
 ;;; .dir-locals.el ends here
